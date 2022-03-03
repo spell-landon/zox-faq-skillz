9ef425dbc3b8fa1b08cf0f25a779724a9b0ee1aa
spell-landon <spell.landon@gmail.com>
2022-03-03 14:04:29 -0600

    Update README, add form fields

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # ZOX FAQ Section
 
- Chekc out the [Live Example](https://zox-faq.netlify.app/) here!
+No live version at the time of writing.
+
+To view on your own computer, clone down and run via your local host:
+
+- If you would like to fork the repository so you have your own copy, feel free to!
+- Click on the green "Code" button.
+- Copy either the HTTPS or SSH link that is provided (SSH is preferred)
+- Open up the Terminal and navigate to the desired directory location
+- Once inside, use the code "`git clone` copied_link_here"
+- Once the repo has been installed, change into the directory with `cd zox-faq-skillz`
+- From here you can run `code .` to open it up in VS Code
+
+> To open in your browser, run `npm start` and that will open up the browser to 'localhost:3000'
 
 ### Example image depicting the FAQ Section of the ZOX 'Contact Us' page
 
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,9 +1,16 @@
 // dependencies
-import React from 'react';
+import React, { useState } from 'react';
 // stylesheet
 import styles from './ContactForm.module.css';
 
 function ContactForm(props) {
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    setFormSubmitted(true);
+  };
+
   return (
     <div className={styles.contactFormContainer}>
       <div className={styles.contactBox}>
@@ -13,10 +20,89 @@ function ContactForm(props) {
           issue within this message so that we are able to help as efficiently
           as possible.
         </p>
-        <form>
+        <form onSubmit={submitForm}>
+          {/* Help Dropdown Selection */}
           <label htmlFor='help'>
             What can we help with today? <span>*</span>
           </label>
+          <select name='help' id='help' required>
+            <option value=''>Please select an option</option>
+            <option value='editOrder'>Edit Order</option>
+            <option value='cancelOrder'>Cancel Order</option>
+            <option value='missingOrder'>Missing Order</option>
+            <option value='incorrectOrder'>Incorrect Order</option>
+            <option value='updateAddressIssue'>Update Address Issue</option>
+            <option value='discountCodeIssue'>Discount Code Issue</option>
+            <option value='subscriptionIssue'>Subscription Issue</option>
+            <option value='otherIssue'>Other Issue</option>
+          </select>
+          {/* First Name */}
+          <label htmlFor='firstName'>
+            First Name <span>*</span>
+          </label>
+          <input
+            type='text'
+            id='firstName'
+            name='firstName'
+            autoComplete='off'
+            required
+          />
+          {/* Last Name */}
+          <label htmlFor='lastName'>
+            Last Name <span>*</span>
+          </label>
+          <input
+            type='text'
+            id='lastName'
+            name='lastName'
+            autoComplete='off'
+            required
+          />
+          {/* Order Number */}
+          <label htmlFor='orderNumber'>
+            Order Number <span>*</span>
+          </label>
+          <input
+            type='text'
+            id='orderNumber'
+            name='orderNumber'
+            autoComplete='off'
+            required
+          />
+          {/* Email */}
+          <label htmlFor='email'>
+            Email <span>*</span>
+          </label>
+          <input
+            type='text'
+            id='email'
+            name='email'
+            autoComplete='off'
+            required
+          />
+          {/* Message */}
+          <label htmlFor='message'>
+            Message <span>*</span>
+          </label>
+          <textarea
+            type='text'
+            id='message'
+            name='message'
+            required
+            placeholder='*max 500 words'
+            cols='10'
+            rows='10'
+          />
+          {/* File Upload */}
+          <label htmlFor='fileUpload'>File Upload (if needed)</label>
+          <textarea
+            type='file'
+            id='fileUpload'
+            name='fileUpload'
+            accept='image/*'
+          />
+          <button type='submit'>Submit</button>
+          {formSubmitted ? <p>Your inquiry has been sent!</p> : null}
         </form>
       </div>
     </div>
